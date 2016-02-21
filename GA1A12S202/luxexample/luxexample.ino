7fea42e19b115228b45e2a4688d84a447d5c044c
arduinolearning <support@arduinolearning.com>
2016-02-21 19:34:31 +0000

    Added files via upload

---
+++ GA1A12S202/luxexample/luxexample.ino
@@ -0,0 +1,20 @@
+#include "GA1A12S202.h"
+
+GA1A12S202 luxValue(A0);
+
+void setup() 
+{
+    Serial.begin(9600);
+    delay(2000);
+}
+
+void loop() 
+{
+    Serial.print("Raw value = ");
+    Serial.print(luxValue.getRaw());
+	Serial.println();
+    Serial.print("Lux value = ");
+    Serial.println(luxValue.getLux());
+	Serial.println();
+    delay(1000);
+}
